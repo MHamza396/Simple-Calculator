b63aa669f934737522b2f5a909c9cec2cdceb76f
MHamza396 <162509142+MHamza396@users.noreply.github.com>
2024-03-24 03:11:28 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+
+let answers = await inquirer.prompt([
+    {message: "Enter First number", type: "number", name: "firstNumber"},
+    {message: "Enter Second number", type: "numer", name: "secondNumber"},
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division",]
+    },
+]);
+if(answers.operator === "Addition"){
+console.log(answers.firstNumber + answers.secondNumber)
+}
+else if(answers.operator === "Subtraction"){
+console.log(answers.firstNumber - answers.secondNumber)
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+  console.log("invalid input");
+}
+
